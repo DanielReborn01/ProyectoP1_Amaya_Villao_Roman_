bec65d231ea72d01cfca284015b7daae084c8f26
DanielReborn01 <wamaya@espol.edu.ec>
2023-11-11 18:15:33 -0500

    Creación de clases

---
+++ src/main/java/com/mycompany/proyectop1_amaya_villao_roman/Vehiculo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectop1_amaya_villao_roman;
+
+/**
+ *
+ * @author danie
+ */
+public class Vehiculo {
+    String placa;
+    String modelo;
+    String marca;
+    boolean esUnAuto;
+    
+    public Vehiculo(String placa, String  modelo, String marca, boolean esUnAuto){
+        this.placa = placa;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.esUnAuto = esUnAuto;
+    }
+    
+}
