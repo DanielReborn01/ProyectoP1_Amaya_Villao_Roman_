78a3f40471140a8f67713a66253017dcc3554f70
DanielReborn01 <wamaya@espol.edu.ec>
2023-11-14 11:51:44 -0500

    Creación de clases

---
+++ src/main/java/com/mycompany/proyectop1_amaya_villao_roman/Entrega.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectop1_amaya_villao_roman;
+
+/**
+ *
+ * @author danie
+ */
+public class Entrega extends Servicio{
+    int numProductos;
+    double peso;
+    TipoEntrega entrega;
+    public void pagoSevicios(double valorPagar, TipoPago pago, TipoEntrega entrega, double peso, int numProductos){
+        
+    }
+}
