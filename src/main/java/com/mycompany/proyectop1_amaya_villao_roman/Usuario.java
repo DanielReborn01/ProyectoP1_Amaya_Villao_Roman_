a88e91fe98406b03f6e38b5a16cb424f8a55f0a2
DanielReborn01 <wamaya@espol.edu.ec>
2023-11-14 11:57:27 -0500

    Creación de clases

---
+++ src/main/java/com/mycompany/proyectop1_amaya_villao_roman/Usuario.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectop1_amaya_villao_roman;
+
+/**
+ *
+ * @author danie
+ */
+public class Usuario {
+    int numCedula;
+    String apellidos;
+    String nombres;
+    String contraseña;
+    int numCelular;
+    
+    public Usuario(){
+        
+    }
+    
+    public Usuario(int numCedula, String apellidos, String nombres, String contraseña, int numCelular){
+        this.numCedula = numCedula;
+        this.apellidos = apellidos;
+        this.nombres = nombres;
+        this.contraseña = contraseña;
+        this.numCelular = numCelular;
+    }
+    public void consultarServicio(){
+        
+    }
+}
