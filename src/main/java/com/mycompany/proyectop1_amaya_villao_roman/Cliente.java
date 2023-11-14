5d076d86340f31d1b5fc5bf486b79455b44a7e0d
DanielReborn01 <wamaya@espol.edu.ec>
2023-11-14 10:52:29 -0500

    Creación de clases

---
+++ src/main/java/com/mycompany/proyectop1_amaya_villao_roman/Cliente.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectop1_amaya_villao_roman;
+
+/**
+ *
+ * @author danie
+ */
+public class Cliente extends Usuario{
+    private int edad;
+    private int numTarjeta;
+
+    /**
+     *
+     * @param numCedula
+     * @param apellidos
+     * @param nombres
+     * @param contraseña
+     * @param numCelular
+     * @param edad
+     * @param numTarjeta
+     */
+    public Cliente(int numCedula, String apellidos, String nombres, String contraseña, int numCelular, int edad, int numTarjeta){
+        super(numCedula, apellidos, nombres, contraseña, numCelular);
+        this.edad = edad;
+        this.numTarjeta = numTarjeta;
+    }
+}
