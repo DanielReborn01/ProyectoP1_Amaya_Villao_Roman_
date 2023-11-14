7ebf86255ec8fb1775743c7e9e04cbdd37878b79
DanielReborn01 <wamaya@espol.edu.ec>
2023-11-14 11:59:38 -0500

    Creación de clases

---
+++ src/main/java/com/mycompany/proyectop1_amaya_villao_roman/TipoEntrega.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.mycompany.proyectop1_amaya_villao_roman;
+
+/**
+ *
+ * @author danie
+ */
+public enum TipoEntrega {
+    MEDICAMENTOS, DOCUMENTOS, ROPA
+}
