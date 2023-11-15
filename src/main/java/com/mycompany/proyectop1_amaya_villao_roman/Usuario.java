92c5c43e1af3e24706eb9312bf11e90dfcb1ffb4
GabrielVillao <GabrielVillao@DESKTOP-QAJJN1M>
2023-11-15 01:31:18 -0500

    getters y setters

--- src/main/java/com/mycompany/proyectop1_amaya_villao_roman/Usuario.java
+++ src/main/java/com/mycompany/proyectop1_amaya_villao_roman/Usuario.java
@@ -26,6 +26,48 @@ public class Usuario {
         this.contraseña = contraseña;
         this.numCelular = numCelular;
     }
+
+    public int getNumCedula() {
+        return numCedula;
+    }
+
+    public void setNumCedula(int numCedula) {
+        this.numCedula = numCedula;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public int getNumCelular() {
+        return numCelular;
+    }
+
+    public void setNumCelular(int numCelular) {
+        this.numCelular = numCelular;
+    }
+    
+    
     public void consultarServicio(){
         
     }
