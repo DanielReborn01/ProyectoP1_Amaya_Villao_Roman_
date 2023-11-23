f491b9f27b9cee8ae3232fe0c6c3678dd5b93b59
GabrielVillao <GabrielVillao@DESKTOP-QAJJN1M>
2023-11-23 03:00:11 -0500

    Sistema_servicio variables

---
+++ src/main/java/com/mycompany/proyectop1_amaya_villao_roman/Sistema_Servicios.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.proyectop1_amaya_villao_roman;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author danie
+ */
+public class Sistema_Servicios {
+    private static List<Usuario>usuario;
+    private static List<Vehiculo>vehiculo;
+    private static List<Servicio>servicio;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
