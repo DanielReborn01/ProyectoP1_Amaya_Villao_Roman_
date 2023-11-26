4ed376d9c8ba9dde8fb0448ccfc5428309d126d7
GabrielVillao <GabrielVillao@DESKTOP-QAJJN1M>
2023-11-26 01:57:10 -0500

    Cambios de clase

--- src/main/java/com/mycompany/proyectop1_amaya_villao_roman/Servicio.java
+++ src/main/java/com/mycompany/proyectop1_amaya_villao_roman/Servicio.java
@@ -5,7 +5,7 @@
 package com.mycompany.proyectop1_amaya_villao_roman;
 
 import java.util.ArrayList;
-
+import Enums.TipoEntrega;
 /**
  *
  * @author danie
@@ -16,6 +16,8 @@ public class Servicio {
     private String fecha;
     private Conductor conductor;
     double ValorPagar;
+    private TipoEntrega tipoentrega;
+    private Cliente cliente;
     
     /**
      * Constructor de la clase Servicio
@@ -23,14 +25,16 @@ public class Servicio {
      * @param destino
      * @param fecha
      * @param conductor
-     * @param valorPagar 
+     * @param valorPagar
+     * @param cliente
      */
-    public Servicio(String origen,String destino, String fecha, Conductor conductor, double valorPagar){
+    public Servicio(String origen,String destino, String fecha, Conductor conductor, double valorPagar, Cliente cliente){
         this.origen=origen;
         this.destino=destino;
         this.fecha = fecha;
         this.conductor = conductor;
         this.ValorPagar = valorPagar;
+        this.cliente=cliente;
     }
     
     
@@ -41,5 +45,55 @@ public class Servicio {
     public void pagoServicio(double valorPagar){
         
     }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public Conductor getConductor() {
+        return conductor;
+    }
+
+    public void setConductor(Conductor conductor) {
+        this.conductor = conductor;
+    }
+
+    public double getValorPagar() {
+        return ValorPagar;
+    }
+
+    public void setValorPagar(double ValorPagar) {
+        this.ValorPagar = ValorPagar;
+    }
+
+    public TipoEntrega getTipoentrega() {
+        return tipoentrega;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+    
+    
     
 }
