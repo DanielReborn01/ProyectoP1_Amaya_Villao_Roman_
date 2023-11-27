2b315a1c7f1ee59a1461dd87dc8994e26a999885
SRoman <SRoman@DESKTOP-VDJSCP4>
2023-11-26 23:26:40 -0500

    asignacion de variables

--- src/main/java/com/mycompany/proyectop1_amaya_villao_roman/Conductor.java
+++ src/main/java/com/mycompany/proyectop1_amaya_villao_roman/Conductor.java
@@ -29,8 +29,9 @@ public class Conductor extends Usuario{
      * @param estado estado del conductor
      * @param vehiculo vehículo del conductor
      */
+    //constructor
     public Conductor(int numCedula,String apellidos, String nombres, String contraseña, int numCelular, TipoUsuario tipoUsuario){
-        super(numCedula,apellidos, nombres, contraseña, numCelular, tipoUsuario);
+        super(numCedula, apellidos, nombres, contraseña, numCelular, tipoUsuario);
         for (String[] dato : datosConductores) {
             if (dato[0].equals(numCedula)) {
                 this.numLicencia = dato[3];
